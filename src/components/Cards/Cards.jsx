52aaefe9d84b1e207eca4e9452dce1a1b4969334
bochnotomas <tomasbochnovic76@gmail.com>
2021-06-03 15:30:42 +0100

    starded working on header component and cards component

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+import cx from 'classnames'
+import CountUp from 'react-countup';
+import {Card, CardContent, Typography, Grid} from '@material-ui/core';
+
+import styles from './Cards.module.css';
+
+
+
+export default function Cards() {
+    return (
+        <div className={styles.container}>
+
+            
+        </div>
+    )
+}
+
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './Header.module.css';
+import cx from 'classnames'
+
+import Cards from '../Cards/Cards';
+
+
+export default function Header() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.title}>
+                <h1 className={styles.title_text}>Cryptozoid</h1>
+                <i className={cx(styles.title_logo, "fas fa-robot")} ></i>
+            </div>
+            
+            <Cards />
+        </div>
+    )
+}
