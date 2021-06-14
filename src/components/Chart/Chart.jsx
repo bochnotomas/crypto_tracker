ae0be95679fe2084069d68c3d2c110ad27458db9
bochnotomas <tomasbochnovic76@gmail.com>
2021-06-14 17:41:22 +0100

    refactored the responsivity of graph

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,9 +3,27 @@ import axios from 'axios';
 import {Line} from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
+function useWindowSize() {
+    const [size, setSize] = useState([window.innerHeight, window.innerWidth]);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSize([window.innerHeight,window.innerWidth]);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+    return size;
+}
+
+
 export default function Chart(props) {
+
     const [prices, setPrices] = useState([]);
     const [timeFormat, setTimeFormat] = useState("200");
+    const [height, width] = useWindowSize();
 
     useEffect(()=> {
         try {
@@ -56,16 +74,29 @@ export default function Chart(props) {
         />) : null
     );
 
-
-    return (
-        <div className={styles.container}>
-            {lineChart}
-            <div className={styles.buttons}>
-                <button className={styles.button} onClick={() => setTimeFormat("1")}>1d</button>
-                <button className={styles.button} onClick={() => setTimeFormat("7")}>7d</button>
-                <button className={styles.button} onClick={() => setTimeFormat("30")}>1m</button>
-                <button className={styles.button} onClick={() => setTimeFormat("360")}>1y</button>
+    if (width < 768 ){
+        return (
+            <div className={styles.container}>
+                {lineChartBig}
+                <div className={styles.buttons}>
+                    <button className={styles.button} onClick={() => setTimeFormat("1")}>1d</button>
+                    <button className={styles.button} onClick={() => setTimeFormat("7")}>7d</button>
+                    <button className={styles.button} onClick={() => setTimeFormat("30")}>1m</button>
+                    <button className={styles.button} onClick={() => setTimeFormat("360")}>1y</button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }else {
+        return (
+            <div className={styles.container}>
+                {lineChart}
+                <div className={styles.buttons}>
+                    <button className={styles.button} onClick={() => setTimeFormat("1")}>1d</button>
+                    <button className={styles.button} onClick={() => setTimeFormat("7")}>7d</button>
+                    <button className={styles.button} onClick={() => setTimeFormat("30")}>1m</button>
+                    <button className={styles.button} onClick={() => setTimeFormat("360")}>1y</button>
+                </div>
+            </div>
+        )
+    }
 }
